7965160ceafc67b1cd7c4ebe96d8e514ac6024b9
Universe Code <martin_cruz_m@hotmail.com>
2019-01-27 14:41:19 -0600

    Formulario Java...

---
+++ FormularioJava/src/clases/Alumno.java
@@ -0,0 +1,68 @@
+
+package clases;
+
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Cruz Medinilla Martín
+ */
+public class Alumno {
+    
+    String nombre;
+    String paterno;
+    String materno;
+    String correo;
+    
+    public Alumno( String nombre, String paterno, String materno, String correo){ 
+        this.nombre = nombre;
+        this.paterno = paterno;
+        this.materno = materno;
+        this.correo = correo;
+    }
+    
+    public String leerAlumnos( ){
+        
+        return "";
+    }
+    
+    public void guardarAlumno(Alumno nuevoAlumno)  throws IOException {
+      
+      String datosAlumno = "";
+      
+      datosAlumno = nuevoAlumno.nombre + " " + nuevoAlumno.paterno + " " + nuevoAlumno.materno + " " + nuevoAlumno.correo + "\n";
+      BufferedWriter writer = new BufferedWriter(new FileWriter("Alumnos.txt"));
+      writer.write(datosAlumno);
+      writer.close();
+      JOptionPane.showMessageDialog(null, "Registro exitoso");
+    }
+    
+    
+    public String muestraContenido(String nombreArchivo) throws FileNotFoundException, IOException {
+
+        String line = null;
+        String alumnos = "";
+
+        try {
+            FileReader fileReader = new FileReader(nombreArchivo);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                alumnos +=line;
+            }   
+
+            bufferedReader.close();         
+        }
+        catch(Exception ex) {
+            System.out.println(ex);                
+        }
+       
+        return alumnos;
+    }
+}
